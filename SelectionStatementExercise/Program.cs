92ef1858b42d0038edd3d812dee96988e0d557e7
Kim Sedlacek <sedlaceks@optonline.net>
2023-11-07 17:51:17 -0500

    selectionstatementsex1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 56;
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low, try again.");
+            }
+                
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($" {userInput} is too high, try again.");
+            }
+               
+            else
+            {
+                Console.WriteLine($"Yes! 56 is my favorite number!");
+            }
+                
+       
+
+
+         
         }
     }
 }
